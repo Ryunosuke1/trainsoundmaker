87d415baa01dfccf20caae3c690c2cf33225ec20
Ryunosuke1 <111185271+Ryunosuke1@users.noreply.github.com>
2022-12-11 01:49:31 +0000

    first commit

---
+++ .vscode/nazo.py
@@ -0,0 +1,30 @@
+import fire
+
+
+
+acc = 0
+class ccli(object):
+    maxspee = 0
+    end2 = 0
+    acceleration = 0
+    def create(self, a, b , d):
+        self.maxspee = a
+        self.end2 = b
+        
+        self.acceleration = d
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+if __name__ == '__main__':
+    fire.Fire(Cli)
+
